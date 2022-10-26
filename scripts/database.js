92b56d28dedeea80fb99d0dc9ef3173170481692
Shaina Couch <shainacouch21@gmail.com>
2022-10-26 13:10:49 -0500

    adding to the databse

---
+++ scripts/database.js
@@ -0,0 +1,230 @@
+const database = {
+    locations: [
+        {
+            id: 1,
+            name: "Muscle Beach",
+            display: 1
+
+        },
+        {
+            id: 2,
+            type: "Great Smokey Mountains",
+            display: 1
+
+        },
+        {
+            id: 3,
+            type: "The Mall of America",
+            display: 1
+
+        },
+        {
+            id: 4,
+            type: "Four Seasons Total Landscaping",
+            display: 1
+
+        },
+        {
+            id: 5,
+            type: "the bathroom",
+            display: 1
+
+        },
+        {
+            id: 6,
+            type: "cloud-base",
+            display: 1,
+
+        },
+        {
+            id: 7,
+            type: "brewery down the road",
+            display: 1
+
+        },
+        {
+            id: 8,
+            type: "the bottom of the Atlantic ocean",
+            display: 1
+
+        },
+        {
+            id: 9,
+            type: "Great Salt Lake",
+            display: 1
+
+        },
+
+    ],
+    historicalfigures: [
+        {
+            id: 1,
+            type: "Ramses II",
+            display: 2
+
+        },
+        {
+            id: 2,
+            type: "John Long",
+            display: 2
+
+        },
+        {
+            id: 3,
+            type: "Big Bird",
+            display: 2
+
+        },
+    ],
+    vehicles: [
+        {
+            id: 9,
+            type: "Great Salt Lake",
+            display: 1
+
+        },
+
+    ],
+
+    animals: [
+
+    ],
+
+    foods: [
+
+    ],
+    timeOfDay: [
+
+        {
+            id: 1,
+            type: "morning",
+            display: 2
+
+        },
+        {
+            id: 2,
+            type: "noon",
+            display: 2
+
+        },
+        {
+            id: 2,
+            type: "night",
+            display: 2
+
+        },
+
+    ],
+    trees : [
+        {
+            id: 1,
+            type: "cedar",
+            display: 1
+
+        },
+        {
+            id: 2,
+            type: "tulip poplar",
+            display: 1
+
+        },
+        {
+            id: 3,
+            type: "maple",
+            display: 1
+
+        },
+        {
+            id: 4,
+            type: "pine",
+            display: 1
+
+        },
+        {
+            id: 5,
+            type: "white oak",
+            display: 1
+
+        },
+        {
+            id: 6,
+            type: "princess",
+            display: 1
+
+        },
+        {
+            id: 7,
+            type: "red oak",
+            display: 1
+
+        },
+        {
+            id: 8,
+            type: "ginco",
+            display: 1
+
+        },
+        {
+            id: 9,
+            type: "beech",
+            display: 1
+
+        },
+        {
+            id: 10,
+            type: "coastal redwood",
+            display: 1
+
+        },
+        {
+            id: 11,
+            type: "sycamore",
+            display: 1
+
+        },
+        {
+            id: 12,
+            type: "ash",
+            display: 1
+
+        },
+        {
+            id: 13,
+            type: "apple",
+            display: 1
+
+        },
+        {
+            id: 14,
+            type: "American chestnut",
+            display: 1
+
+        },
+    ],
+    locations: [
+        {
+            id: 1,
+
+        }
+    ],
+    locations: [
+        {
+            id: 1,
+
+        }
+    ],
+    locations: [
+        {
+            id: 1,
+
+        }
+    ],
+    locations: [
+        {
+            id: 1,
+
+        }
+    ],
+
+
+}
