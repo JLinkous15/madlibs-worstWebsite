c92d0a665451c22a39e889eba6e5e45ba29a6131
Shaina Couch <shainacouch21@gmail.com>
2022-10-27 15:50:12 -0500

    animals - drinkingglass

--- scripts/Topics/DrinkingGlass.js
+++ scripts/Topics/DrinkingGlass.js
@@ -1,3 +1,26 @@
-import { getDrinkingGlasses } from "./database.js"
+import { getDrinkingGlasses } from "../database.js"
 
 const drinkingGlass = getDrinkingGlasses() 
+
+export const drinkingGlassHTML = () => {
+    
+    let html = `<label for="drinkingGlass">Choose a Drinking Glass:</label>
+    `
+    if(drinkingGlass.length <= 3){
+        html += `<ul>`
+        const tempHTML = drinkingGlass.map(glass=>
+            `<li><input type="radio" name="drinkingGlass" value="${glass.id}"/> ${glass.type}</li>`
+            )
+        html += `${tempHTML.join("")}</ul>`
+    } else {
+        html += `<select id="drinkingGlass" name="drinkingGlass">
+        <option value="0">Prompt to select resource...</option>`
+
+        const tempHTML = drinkingGlass.map(glass=>
+            `<option value="${glass.id}">${glass.type}</option>`)
+            
+        html += `${tempHTML.join("")}</select>`
+    }
+    
+    return html
+}
