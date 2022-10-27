38fefe262ee10d1a8bcded76f80a6760f21dc93b
lohanson33@gmail.com <lohanson33@gmail.com>
2022-10-27 09:26:45 -0500

    First main.js commit

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+import { MadLibs } from "./madlibs.js"
+
+const container = document.querySelector("#mainContainer") 
+
+const renderAllHTML = () => { 
+    container.innerHTML = MadLibs()
+}
+
+renderAllHTML()
